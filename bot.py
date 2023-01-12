ce688746a90aa47768228b20e8d3f3f7c4a6c171
Udnek <gleb.dudorov@gmail.com>
2023-01-12 11:59:30 +0300

    First bot

---
+++ bot.py
@@ -0,0 +1,31 @@
+import logging
+import settings
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+logging.basicConfig(filename='bot.log', level=logging.INFO)
+
+
+def greet_user(update, context):
+    print('Вызван /start')
+    update.message.reply_text('Нюхай бебру')
+
+
+def talk_to_me(update, context):
+    user_text = update.message.text
+    print(user_text)
+    update.message.reply_text(user_text)
+
+
+def main():
+    mybot = Updater(settings.API_KEY, use_context=True)
+
+    dispatcher = mybot.dispatcher
+    dispatcher.add_handler(CommandHandler("start", greet_user))
+    dispatcher.add_handler(MessageHandler(Filters.text, talk_to_me))
+
+    logging.info("Бот стартовал")
+    mybot.start_polling()
+    mybot.idle()
+
+
+if __name__ == "__main__":
+    main()
